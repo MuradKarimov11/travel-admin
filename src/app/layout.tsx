3dff2e4d76cdf1c2de22871f486a9406b4595334
Murad Karimov <muradkarimov07@gmail.com>
2025-11-05 13:02:52 +0300

    add sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
 import ClientProvider from "@/components/providers/ClientProvider";
 
 const geistSans = Geist({
